a9ecfe027cbf285a657e738b7d24ffca0ccd17a6
jdgies <jennifer.gies@shaw.ca>
2016-06-13 11:20:07 -0600

    Consistency updates following completion of Plot2
    Updated plot1 code to be more consistent with plot2

--- plot1.R
+++ plot1.R
@@ -9,7 +9,6 @@ if(!file.exists(destfile)) {
         download.file(url,destfile=destfile, mode="wb")
         setwd("./data")
         unzip("power.zip")
-        setwd("~/Training/04 Exploratory Data Analysis")
 }
 
 print("Loading Data...")
@@ -17,13 +16,13 @@ fileName = "./data/household_power_consumption.txt"
 power <- read.table(fileName, sep=";", header=TRUE, stringsAsFactors = TRUE, na.strings = "?")
 
 print("Subsetting Data...")
-power$Date <- strptime(as.character(power$Date), format = "%d/%m/%Y")
-power <- subset(power, Date >= "2007-02-01" & Date <= "2007-02-02")
+power <- power[(power$Date=="1/2/2007" | power$Date=="2/2/2007"),]
 
 ############ Plot 1 ############
 print("Creating plot...")
+png("./data/plot1.png", width = 480, height = 480)
 with(power, hist(Global_active_power, col = "red", main = "Global Active Power", 
                  xlab = "Global Active Power (kilowatts)"))
-print("Saving plot as plot1.png...")
-dev.copy(png, file="plot1.png", width=480, height=480)
+#print("Saving plot as plot1.png...")
+#dev.copy(png, file="plot1.png", width=480, height=480)
 dev.off()
